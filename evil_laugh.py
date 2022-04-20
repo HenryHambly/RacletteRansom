e84b4d03890f4bedfe27e405deeea70e86192f7f
HenryHambly <104066815+HenryHambly@users.noreply.github.com>
2022-04-20 12:17:22 +0200

    Create evil_laugh.py

---
+++ evil_laugh.py
@@ -0,0 +1,9 @@
+from ransom import *
+from cheese import raclette
+
+while universe_exist == True:
+    ransom(every_non-swiss_cheeese_dactory)
+    # cause we are a swiss ransom gang
+    raclette.eat()
+    
+  # Mouahahaha
