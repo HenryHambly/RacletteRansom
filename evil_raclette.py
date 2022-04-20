c81960c3e290b56eedea17e90a4ebbebe70b2640
HenryHambly <104066815+HenryHambly@users.noreply.github.com>
2022-04-20 12:19:12 +0200

    Update evil_raclette.py

--- evil_raclette.py
+++ evil_raclette.py
@@ -2,7 +2,7 @@ from ransom import *
 from cheese import raclette
 
 while universe_exist == True:
-    ransom(every_non-swiss_cheeese_dactory())
+    ransom(every_non_swiss_cheeese_dactory())
     # cause we are a swiss ransom gang
     raclette.eat()
     
